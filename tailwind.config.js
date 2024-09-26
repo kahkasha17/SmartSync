32e2d23a7baabd00f2b85387e04d04fe1c04a964
Kahkasha17 <kahkashakhan1417@gmail.com>
2024-09-26 12:31:27 +0530

    tailwind add successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./node_modules/flowbite/**/*.js"],
+  content: [
+    './*.html',    // Look for classes in all HTML files in the root directory
+    './src/**/*.html',  // Look for classes in all HTML files in the `src` directory (if you have a folder)
+    './src/**/*.js',    // Look for classes in any JS files within the `src` directory
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin"),
-    require('tailwindcss'),
-    require('autoprefixer'),
+  plugins: [],
+}
 
-    
-
-
-  ],
-};
